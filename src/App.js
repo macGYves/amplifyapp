80615c4e84318e1d8403d04015f4a5a50e61bfdf
Yves Gänssinger <macgyves@gaenssinger.de>
2024-09-18 21:21:51 +0200

    v3

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Do it now!
+          Learn React. Do it now! Aber zacki!
         </a>
       </header>
     </div>
